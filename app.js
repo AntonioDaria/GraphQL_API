efeb254f5bcdbcf77dbac224f3f1cfd07b832894
Antonio D'aria <antoniod@IM-SOLUTIONS.net>
2021-01-21 16:45:59 +0000

    WIP back-end API schema for book and author objects

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
+
+const app = express()
+
+// app.use('/graphql', (req, res) => {
+// 	graphqlHTTP({
+//   		schema: Schema,
+//   		rootValue: req,
+//   		graphiql: true
+// 	},{
+// 		debug : false
+// 	})(req, res)
+// });
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}));
+
+app.listen(4000, () =>{
+    console.log('Listening for request on port 4000')
+})
+
+
+//1st step create express server app
+//2nd Create a graphqlHTTP middleware and pass a schema to it
